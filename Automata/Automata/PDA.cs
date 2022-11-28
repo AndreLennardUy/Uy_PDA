1dd81610b5bc21dd0965f3816d41dcdd7ff68c4d
Andre Lennard S. Uy <andrelennard.uy@cit.edu>
2022-11-28 19:05:30 +0800

    Add balance symbol function and palindrome checker

---
+++ Automata/Automata/PDA.cs
@@ -0,0 +1,97 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Automata
+{
+    public class PDA
+    {
+        public bool CheckSymbol(String inputs)
+        {
+            Stack<char> stack = new Stack<char>();
+            int len = inputs.Length;
+
+            for (int i = 0; i < len; i++)
+            {
+                switch (inputs[i])
+                {
+                    case '(':
+                        stack.Push(inputs[i]);
+                        break;
+                    case ')':
+                        if (stack.Peek() == '(')
+                        {
+                            stack.Pop();
+                        }else
+                        {
+                            return false;
+                        }
+                        break;
+                    case '{':
+                        stack.Push(inputs[i]);
+                        break;
+                    case '}':
+                        if (stack.Peek() == '{')
+                        {
+                            stack.Pop();
+                        }else
+                        {
+                            return false;
+                        }
+                        break;
+                    case '[':
+                        stack.Push(inputs[i]);
+                        break;
+                    case ']':
+                        if (stack.Peek() == '[')
+                        {
+                            stack.Pop();
+                        }else
+                        {
+                            return false;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            
+            if (stack.Count() == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool CheckPalindrome(String word)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            int len = word.Length, i;
+
+            for (i = 0; i < len / 2; i++)
+            {
+                stack.Push(word[i]);
+            }
+            if (len % 2 != 0)
+            {
+                i = i + 1;
+            }
+
+            while (i < len)
+            {
+                char c = stack.Peek();
+                stack.Pop();
+
+                if (c != word[i])
+                {
+                    return false;
+                }
+                i++;
+            }
+            return true;
+        }
+    }
+}
---
+++ Automata/Automata/Program.cs
@@ -0,0 +1,46 @@
+﻿using Automata;
+using System.IO.Pipes;
+
+PDA pda = new PDA();
+int ans;
+do
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("Which one do you like to test\n0: Terminate\n1: Symbol test\n2:Palindrome");
+    ans = Convert.ToInt32(Console.ReadLine());
+
+    if(ans == 1)
+    {
+        Console.WriteLine("Enter string");
+        string input = Console.ReadLine();
+
+
+        if ( pda.CheckSymbol(input))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Balance"); 
+        } else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Not Balance");
+        }
+    } else if(ans == 2)
+    {
+        Console.WriteLine("Enter string");
+        string input = Console.ReadLine();
+
+
+        if (pda.CheckPalindrome(input))
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Palindrome!");
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Not Palindrome");
+        }
+        
+    } 
+
+} while (ans != 0);
